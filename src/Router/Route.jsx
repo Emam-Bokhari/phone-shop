423763f6c37fd4accba66019288226a906ae9143
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2023-09-23 11:14:20 +0600

    Mainlayout

---
+++ src/Router/Route.jsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
+import Favourites from "../pages/Favourites/Favourites";
+
+const Route = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/favourites",
+                element: <Favourites />
+            },
+            {
+                path:"/login"
+            }
+        ]
+    }
+])
+export default Route
---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,56 @@
+
+import { Link, NavLink } from "react-router-dom";
+
+const MainLayout = () => {
+    return (
+        <div>
+
+            <section className="max-w-screen-xl mx-auto px-8 md:px-16 my-10">
+                <div className="flex justify-evenly items-center shadow-md py-4 rounded-md">
+                    <div>
+                        <h2 className="text-2xl font-semibold">Phone Shop</h2>
+                    </div>
+
+                    <nav>
+                        <ul className="flex gap-5 text-xl">
+                            <li>
+                                <NavLink
+                                    to="/"
+                                    className={({ isActive, isPending }) =>
+                                        isPending ? "pending" : isActive ? "underline text-green-500" : ""
+                                    }
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/favourites"
+                                    className={({ isActive, isPending }) =>
+                                        isPending ? "pending" : isActive ? "underline text-green-500" : ""
+                                    }
+                                >
+                                   Favourites
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/login"
+                                    className={({ isActive, isPending }) =>
+                                        isPending ? "pending" : isActive ? "underline text-green-500" : ""
+                                    }
+                                >
+                                  Login
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </section>
+
+        </div>
+    );
+};
+
+export default MainLayout;
+
